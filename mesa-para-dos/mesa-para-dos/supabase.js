20d2226a733efc35035b538129ffd46a08e057e6
ikerysandri <iiikeeergl@gmail.com>
2026-05-21 23:16:59 +0200

    Delete mesa-para-dos/mesa-para-dos directory

--- mesa-para-dos/mesa-para-dos/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseKey = import.meta.env.API_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
